081d63985e56fb16579bae0f2029147186ff8e91
Gopal Kumar Singh <gopalkumarsingh7007@gmail.com>
2025-07-25 22:18:30 +0530

    location changed

---
+++ Tailwind Exercise/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        '900': '900px',
+      }
+    }
+  }
+}
